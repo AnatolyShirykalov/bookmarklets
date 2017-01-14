f4abb3536201553899ccbb97372133bddf457a5d
AnatolyShirykalov <pipocavsobake@gmail.com>
2017-01-14 16:35:29 +0300

    restrict to play two players

--- classic-online.autoload.js
+++ classic-online.autoload.js
@@ -29,3 +29,11 @@ $($('ul.paging')[0].children).filter((index, elem)=>
     }
   })
 );
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
